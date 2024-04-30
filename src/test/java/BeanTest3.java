8733b36155568ec32db16fa55f2e71fd169961f4
FruitSalvage <1847997653@qq.com>
2024-04-30 12:46:34 +0800

    Initial commit

--- src/test/java/BeanTest3.java
+++ src/test/java/BeanTest3.java
@@ -4,7 +4,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class BeanTest3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext classPathXmlApplicationContext = new ClassPathXmlApplicationContext("applicationBean3.xml");
-        Bean3 bean3 = (Bean3)classPathXmlApplicationContext.getBean("bean3");
+//        Bean3 bean3 = (Bean3)classPathXmlApplicationContext.getBean("bean3");
+        Bean3 bean3 = classPathXmlApplicationContext.getBean("bean3", Bean3.class);
         System.out.println(bean3);
     }
 }
